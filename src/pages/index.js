4b6bcd79a1e861de1587633450c36894573e1886
szabolcsit <szabolcsit@hu.ibm.com>
2018-11-01 08:53:27 +0100

    we have a date!

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,13 @@ import styles from './index.module.scss'
 
 const IndexPage = () => (
   <Layout className={styles.home}>
-    <Helmet meta={[{ name: 'foo', content: 'bar' }]} />
     <h1 className={styles.date}>
-      2019 <span className={styles.month}>September</span>
+      2019 <span className={styles.month}>September</span> <span className={styles.days}>26-27</span>
     </h1>
+    <div className={styles.info}>
+      Ticket sales & Call For Papers<br />
+      start in December 2018
+    </div>
   </Layout>
 )
 
