44f4e431b84afdb52ffd68f853a9c22222be7f8d
szabolcsit <szabolcsit@hu.ibm.com>
2018-12-01 08:25:52 +0100

    cfp & impressum page

---
+++ src/pages/call-for-speakers.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import MainLayout from '../components/MainLayout/index'
+import styles from './simpletext.module.scss'
+
+const CallForSpeakers = () => (
+  <MainLayout>
+    <div className={styles.simpleText}>
+      <h1>JSConf Budapest 2019 Call for Speakers</h1>
+      <p>
+        All attendees, speakers, sponsors and volunteers at JSConf Budapest are
+        required to agree with the following code of conduct. Organizers will
+        enforce this code throughout the event. We are expecting coorporation from
+        all participants to help ensuring a safe environment for everybody.
+      </p>
+      
+    </div>
+  </MainLayout>
+)
+
+export default CallForSpeakers
---
+++ src/pages/impressum.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainLayout from '../components/MainLayout/index'
+import styles from './simpletext.module.scss'
+
+const Impressum = () => (
+  <MainLayout>
+    <div className={styles.simpleText}>
+      <h1>Impressum</h1>
+      
+    </div>
+  </MainLayout>
+)
+
+export default Impressum
