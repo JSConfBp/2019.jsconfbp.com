e3dff0fc760ab9f9c12e08ac3e1172aca429d958
Benedek Gagyi <gagyibenedek@yahoo.com>
2018-12-03 09:24:57 +0100

    add more details

--- src/pages/call-for-speakers.js
+++ src/pages/call-for-speakers.js
@@ -99,20 +99,27 @@ const CallForSpeakers = () => (
       </p>
       
       <h3>Guidelines</h3>
-
-Submit your proposal by February 28th 2019, 23:59:59 CEST.
-All talks are in English.
-
-Talks are usually 30 minutes long (for longer talks we’d get in touch with you directly). There will be no Q&A after the talks - we encourage attendees and speakers to talk to each in the breaks, after the sessions. 
-
-Make sure you care, and make sure we see you care. Typos, sloppy formatting and all-lowercase submissions make our reading of your proposal tedious. These things will definitely count against your proposal.
-
-Don’t overdo it either. If you need more than two paragraphs to get to the point of your topic, we need to ask you to slim things down. With the amount of submissions we get, the quicker you can to make a good impression, the better.
-
-Original Topics. One of the things we like to do with JSConf Budapest is to push the community forward. We can’t do this if the same people keep talking about the same things all the time. Thus, we favor original content. If you want to discuss a topic that you have talked about elsewhere, try to add a twist, or new research, or development, something unique. Of course, if your talk is plain awesome as-is, go for that :)
-
-Simply assume this: the audience has infinite intelligence but zero knowledge.
-
+      <p>
+        Submit your proposal by <strong>February 28th 2019, 23:59:59 CEST</strong>.
+      </p>
+      <p>
+        All talks are in English.
+      </p>
+      <p>
+        Talks are usually 30 minutes long (for longer talks we’d get in touch with you directly). There will be no Q&A after the talks - we encourage attendees and speakers to talk to each in the breaks, after the sessions. 
+      </p>
+      <p>
+        Make sure you care, and make sure we see you care. Typos, sloppy formatting and all-lowercase submissions make our reading of your proposal tedious. These things will definitely count against your proposal.
+      </p>
+      <p>
+        Don’t overdo it either. If you need more than two paragraphs to get to the point of your topic, we need to ask you to slim things down. With the amount of submissions we get, the quicker you can to make a good impression, the better.
+      </p>
+      <p>
+        Original Topics. One of the things we like to do with JSConf Budapest is to push the community forward. We can’t do this if the same people keep talking about the same things all the time. Thus, we favor original content. If you want to discuss a topic that you have talked about elsewhere, try to add a twist, or new research, or development, something unique. Of course, if your talk is plain awesome as-is, go for that :)
+      </p>
+      <p>
+        Simply assume this: the audience has infinite intelligence but zero knowledge.
+      </p>
 
       <h3>Selection Process</h3>
 
@@ -141,17 +148,30 @@ Simply assume this: the audience has infinite intelligence but zero knowledge.
 
       <h3>We are here to help!</h3>
 
-      Not everybody is a natural talent on stage. Not everybody can produce kick-ass slide-decks. Not everybody is a live-demo-god. Not everybody knows they have something great to talk about.
-There are about a million reasons why you don’t consider yourself a speaker. We are here to prove you wrong. If all you have is a gut feeling that you should be on stage, we are here to reach out and help you to develop or hone the skills you think you lack to deliver a great presentation.
+      <p>
+        Not everybody is a natural talent on stage. Not everybody can produce kick-ass slide-decks. Not everybody is a live-demo-god. Not everybody knows they have something great to talk about.
+        There are about a million reasons why you don’t consider yourself a speaker. 
+        <br />
+        We are here to prove you wrong. 
+        <br />
+        If all you have is a gut feeling that you should be on stage, we are here to reach out and help you to develop or hone the skills you think you lack to deliver a great presentation.
+      </p>
+      <p>
+        We are happy to brainstorm your interests to see if a great topic is hiding.
+        <br />
+        We are happy to connect you with experienced speakers to help prepare your submission
+        <br />
+        We are happy to review and advise on how to produce a slide deck.
+      </p>
 
-We are happy to brainstorm your interests to see if a great topic is hiding.
-We are happy to connect you with experienced speakers to help prepare your submission
-We are happy to review and advise on how to produce a slide deck.
-
-If you need practice giving talks, get in touch, we can hook you up with local groups.
-Again, whatever else you might need, we’re here to help.
-
-Get in touch: team@jsconfbp.com (just please don’t use this address to submit a proposal).
+      <p>
+        If you need practice giving talks, get in touch, we can hook you up with local groups.
+        <br />
+        Again, whatever else you might need, we’re here to help.
+      </p>
+      <p>
+        Get in touch: <strong>team@jsconfbp.com</strong> (just please don’t use this address to submit a proposal).
+      </p>  
 
       <CFP_Button />
     </div>
