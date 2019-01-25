99a758940b79e394fab6dbb561c67f4efa666ee0
szabolcsit <szabolcsit@hu.ibm.com>
2019-01-25 17:51:09 +0100

    fix breakpoints

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -3,8 +3,11 @@ import { Link } from 'gatsby'
 import styles from './logo.module.scss'
 import LogoImage from './image'
 
-const Logo = ({menuOpen}) => (
-  <Link className={[styles.logo, (menuOpen ? styles.logo_menu_open : '')].join(' ')} to="/">
+const Logo = ({ menuOpen }) => (
+  <Link
+    className={[styles.logo, menuOpen ? styles.logo_menu_open : ''].join(' ')}
+    to="/"
+  >
     <LogoImage className={styles.inner} />
   </Link>
 )
