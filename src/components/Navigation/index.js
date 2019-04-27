706145550dd4d70aa7d5a6555f3778982a862096
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2019-04-27 13:50:34 +0200

    rough schedule

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -14,12 +14,13 @@ const Navigation = props => {
   return (
     <nav className={className.join(' ')}>
       <ul>
-        {/*
+
         <li className={styles.item}>
-          <Link className={styles.link} to="/call-for-speakers">
-            Call for Speakers
+          <Link className={styles.link} to="/schedule">
+            Schedule
           </Link>
         </li>
+        {/*
         <li className={styles.item}>
           <Link className={styles.link} to="/scholarship-program">
             Scholarship
---
+++ src/pages/schedule.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import Layout from '../components/Layout/index'
+import styles from './simpletext.module.scss'
+import SocialMeta from '../components/SocialMeta/index'
+
+const Schedule = () => (
+  <Layout>
+    <SocialMeta image="social_card.jpg" />
+    <div className={styles.simpleText}>
+		<h1>
+			Schedule
+		</h1>
+
+		<p>
+			This will be the rough schedule for September, we'll get this more and more detailed with time.
+		</p>
+
+		<h3>Tuesday, 24th September, 2019</h3>
+
+		<p>
+			<strong>Workshop day</strong> - we'll annouce any details as soon as possible!
+		</p>
+
+		<h3>Wednesday, 25th September, 2019</h3>
+
+		<p>
+			<strong>CSSConf Budapest 2019</strong>
+		</p>
+
+		<ul>
+			<li>Breakfast</li>
+			<li>Event opening</li>
+			<li>Coffee break</li>
+			<li>Lunch</li>
+			<li>Coffee break</li>
+			<li>Event closing</li>
+			<li>Dinner & Afterparty</li>
+		</ul>
+
+		<h3>Thursday, 26th September, 2019</h3>
+
+		<p>
+			<strong>JSConf Budapest 2019 Day 1</strong>
+		</p>
+
+		<ul>
+			<li>Breakfast</li>
+			<li>Event opening</li>
+			<li>Coffee break</li>
+			<li>Lunch</li>
+			<li>Coffee break</li>
+			<li>Dinner</li>
+			<li>Party on Day one </li>
+		</ul>
+
+		<h3>Friday, 27th September, 2019</h3>
+
+		<p>
+			<strong>JSConf Budapest 2019 Day 2</strong>
+		</p>
+
+		<ul>
+			<li>Breakfast</li>
+			<li>Coffee break</li>
+			<li>Lunch</li>
+			<li>Coffee break</li>
+			<li>Event closing</li>
+			<li>Afterparty</li>
+		</ul>
+
+    </div>
+  </Layout>
+)
+
+export default Schedule
