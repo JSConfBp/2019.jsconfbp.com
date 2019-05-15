3ce9cc09d501e260dad6ebe62148fde6939e42e2
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2019-05-15 11:21:42 +0200

    Update workshops.js

--- src/pages/workshops.js
+++ src/pages/workshops.js
@@ -5,15 +5,24 @@ import SocialMeta from '../components/SocialMeta/index'
 
 import WorkshopList from '../components/WorkshopList'
 
-const Schedule = props => (
+const Workshops = props => (
   <Layout>
     <SocialMeta image="social_card_workshops.jpg" />
     <div className={styles.workshops}>
       <h1 className={styles.typo}>Workshops</h1>
 
       <p className={styles.typo}>
-        This will be the rough schedule for September, we'll get this more and
-        more detailed with time.
+        <strong>These workshops are FREE for every ticket holder of JSConf Budapest or a Combo ticket with CSSConf Budapest.</strong>
+      </p>
+
+      <p className={styles.typo}>
+        The workshops have limited seats, so be sure to <a href="https://ti.to/jsconf-bp/jsconf-budapest-2019">get your ticket</a>
+        before they fill up! We'll open them slowly, so everyone have a chance to pick a workshop they like.
+      </p>
+
+      <p className={styles.typo}>
+        You will be able to register to a workshop using the ID on your ticket. If will be possible to switch the workshop
+        you've picked later, if there is a free seat available on another workshop.
       </p>
 
       <WorkshopList data={props.data} />
@@ -50,4 +59,4 @@ export const query = graphql`
   }
 `
 
-export default Schedule
+export default Workshops
