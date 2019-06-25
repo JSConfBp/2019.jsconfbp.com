dbcbc0ba6263dbe2daa81987d4296f030f5a2d69
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2019-06-25 09:51:59 +0200

    fix perk styles

--- src/components/Sponsoration/Perks/index.js
+++ src/components/Sponsoration/Perks/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import Tooltip from '@material-ui/core/Tooltip'
 import InfoIcon from '@material-ui/icons/Info'
 import { withStyles } from '@material-ui/core/styles'
@@ -17,64 +17,73 @@ const customStyles = {
     color: theme.palette.primary,
   },
   popper: {
-    fontSize: '2rem'
-  }
+    fontSize: '2rem',
+  },
 }
 
 const perk_list = [
   {
     name: 'coffee',
     label: 'Specialty Coffee',
-    info: 'High quality coffee by Espresso Embassy Budapest, branded with your company',
-    price: '€4,000'
+    info:
+      'High quality coffee by Espresso Embassy Budapest, branded with your company',
+    price: '€4,000',
   },
   {
     name: 'party1',
     label: 'CSS Closing Party',
-    info: 'Vast chance to present company during the event and form party face for serving your purposes. Attendees will have better experience with a sposorated party, filled with activities not just drinking and talking',
-    price: '€5,000'
+    info:
+      'Vast chance to present company during the event and form party face for serving your purposes. Attendees will have better experience with a sposorated party, filled with activities not just drinking and talking',
+    price: '€5,000',
   },
   {
     name: 'party2',
     label: 'First Night Party',
-    info: 'Vast chance to present company during the event and form party face for serving your purposes. Attendees will have better experience with a sposorated party, filled with activities not just drinking and talking',
-    price: '€5,000'
+    info:
+      'Vast chance to present company during the event and form party face for serving your purposes. Attendees will have better experience with a sposorated party, filled with activities not just drinking and talking',
+    price: '€5,000',
   },
   {
     name: 'dinner',
     label: 'Dinner',
-    info: 'Presence of company everywhere which is connected to meals and good branding opportunity with healthy vegan food plus vegetarian & meat opt-in.',
-    price: '€4,000'
+    info:
+      'Presence of company everywhere which is connected to meals and good branding opportunity with healthy vegan food plus vegetarian & meat opt-in.',
+    price: '€4,000',
   },
   {
     name: 'party3',
     label: 'JS Closing Party',
-    info: 'Vast chance to present company during the event and form party face for serving your purposes. Attendees will have better experience with a sposorated party, filled with activities not just drinking and talking',
-    price: '€5,000'
+    info:
+      'Vast chance to present company during the event and form party face for serving your purposes. Attendees will have better experience with a sposorated party, filled with activities not just drinking and talking',
+    price: '€5,000',
   },
   {
     name: 'wifi',
     label: 'Wifi Pass',
-    info: 'Your brand or keyword is the wifi pass, attendees, who need internet, has to type it in at least once',
-    price: '€3,000'
+    info:
+      'Your brand or keyword is the wifi pass, attendees, who need internet, has to type it in at least once',
+    price: '€3,000',
   },
   {
     name: 'guitarhero',
     label: 'Guitar Hero',
-    info: 'The Venue has a wall with a huge LED screen formed as a guitar, you can create a video that will run on that screen in a loop, if it catchy enough, attendees can use it as a selfie wall',
-    price: '€2,000'
+    info:
+      'The Venue has a wall with a huge LED screen formed as a guitar, you can create a video that will run on that screen in a loop, if it catchy enough, attendees can use it as a selfie wall',
+    price: '€2,000',
   },
   {
     name: 'sketchnotes',
     label: 'Sketch Notes',
-    info: 'These drawings could be shared on social media with your hashtag or twitter handle attached - the drawing itself can be branded with your logo in a corner, or top center',
-    price: '€2,000'
+    info:
+      'These drawings could be shared on social media with your hashtag or twitter handle attached - the drawing itself can be branded with your logo in a corner, or top center',
+    price: '€2,000',
   },
   {
     name: 'lanyrd',
     label: 'Lanyrd',
-    info: 'Your logo next to the JSConf Budapest / CSSConf Budapest logo on lanyards',
-    price: '€2,000'
+    info:
+      'Your logo next to the JSConf Budapest / CSSConf Budapest logo on lanyards',
+    price: '€2,000',
   },
 ]
 
@@ -84,8 +93,12 @@ const PerkList = () => (
       <li key={`perk_${perk.label}`}>
         <strong className={styles.perk_label}>
           {perk.label}
-          <Tooltip title={perk.info} placement="right" classes={{ popper: styles.popper }}>
-            <InfoIcon color="primary" className={ styles.perk_icon } />
+          <Tooltip
+            title={perk.info}
+            placement="right"
+            classes={{ popper: styles.popper }}
+          >
+            <InfoIcon color="primary" className={styles.perk_icon} />
           </Tooltip>
         </strong>
         <span className={styles.perk_value}>{perk.price}</span>
