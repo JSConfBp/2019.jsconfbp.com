7ac162c4db9f59b19ac9729909f65e24ca65a633
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2019-10-16 09:49:54 +0200

    fix embeds

--- src/components/YoutubeVideo/index.js
+++ src/components/YoutubeVideo/index.js
@@ -7,6 +7,7 @@ const YoutubeVideo = props => (
     <iframe
       src={props.src}
       frameBorder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title={props.title}
     />
